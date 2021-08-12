1eaa272b4f0ea59e5e1e195f8be291582bb8442c
vincentzreo <1978892854@qq.com>
2021-08-12 16:24:38 +0800

    init

---
+++ StubPlayerStore.go
@@ -0,0 +1,15 @@
+package main
+
+type StubPlayerStore struct {
+	scores map[string]int
+	winCalls []string
+	league []Player
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
+}
+func (s *StubPlayerStore) RecordWin(name string) {
+	s.winCalls = append(s.winCalls, name)
+}
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+
+
+func main() {
+
+	server := NewPlayerServer(NewInMemoryPlayerStore())
+	if err := http.ListenAndServe(":5000", server); err != nil{
+		log.Fatalf("Could not listen on port 5000 %v", err)
+	}
+}
